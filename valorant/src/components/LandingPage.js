0376d336c7ca4ae5c5fc9cd907ce49603e52fa71
CHR-onicles <tpandivine48@gmail.com>
2021-06-24 16:20:40 +0000

    Revert "attempting full background video while displaying landing page text"
    This reverts commit 4117152a512f70af63fa4a96af6e91db51a9fa8f.

--- valorant/src/components/LandingPage.js
+++ valorant/src/components/LandingPage.js
@@ -1,31 +1,32 @@
-import React from "react";
-import "./LandingPage.css";
-import ValorantVid from "../assets/ValorantVid.mp4";
-import ValorantLogo from "../assets/Valorant logo.png";
+import React from 'react'
+import "./LandingPage.css"
+import ValorantVid from "../assets/ValorantVid.mp4"
+import ValorantLogo from "../assets/Valorant logo.png"
 
 const LandingPage = () => {
-  return (
-    <div className="lp-master">
-      <div className="lp">
-        <video id="myVideo" autoPlay loop muted>
-          <source src={ValorantVid} type="video/mp4" />
-        </video>
+    return (
+        <div className="lp-master" >
+            <div className="lp" >
 
-        <div className="lp-items">
-          <p>A 5v5 character-based tactical shooter</p>
+                <video  id="myVideo" autoPlay loop muted >
+                    <source  src={ValorantVid} type="video/mp4" />
+                </video>
 
-          <img src={ValorantLogo} alt="" />
+                <div className="lp-items" > 
+                    <p>A 5v5 character-based tactical shooter</p>    
+
+                    <img src={ValorantLogo} alt="" />                      
+                </div>
+                      
+            </div>
+            <div className="cta" >
+                <a href=""><button>PLAY FREE</button></a> 
+            </div>
+
+            <div className="vl-1" ></div>              
+            
         </div>
-      </div>
-      <div className="cta">
-        <a href="">
-          <button>PLAY FREE</button>
-        </a>
-      </div>
+    )
+}
 
-      {/* <div className="vl-1" ></div>               */}
-    </div>
-  );
-};
-
-export default LandingPage;
+export default LandingPage
